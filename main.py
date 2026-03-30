4ec99e2f1a47bd46eff931c7fb3fbdca883433b8
xzion35 <dylan.mer@hotmail.com>
2026-03-30 21:51:16 +0200

    Added Sound

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import sys
 import chess
-import time
 import chess.pgn
 import random
 
 # GUI
 from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget, QFileDialog
 from PySide6.QtGui import QAction, QIcon
+from PySide6.QtCore import QUrl, QTimer
+from PySide6.QtMultimedia import QSoundEffect
 from chess_widgets import BoardWidget
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         # Game state
+        self.variation = []
         self.variations = []
         self.expected_move = None
         self.opening = None       
@@ -34,6 +36,15 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(container)
         self.hint_button.clicked.connect(self.show_move)
 
+        # Sound
+        self.move_sound = QSoundEffect()
+        self.move_sound.setSource(QUrl.fromLocalFile("Sound/Move.wav"))
+        self.capture_sound = QSoundEffect()
+        self.capture_sound.setSource(QUrl.fromLocalFile("Sound/Capture.wav"))
+        
+        self.move_sound.setVolume(0.5)
+        self.capture_sound.setVolume(0.5)
+        
         # Connect signal
         self.board_widget.move_played.connect(self.on_move_played)
 
@@ -56,6 +67,12 @@ class MainWindow(QMainWindow):
         reset_action = QAction("Reset Board", self)
         reset_action.triggered.connect(self.reset_board)
         file_menu.addAction(reset_action)
+
+    def play_move_sound(self, move: chess.Move):
+        if self.board.is_capture(move):
+            self.capture_sound.play()
+        else:
+            self.move_sound.play()
 
     def open_pgn(self):
         file_path, _ = QFileDialog.getOpenFileName(self, "Open PGN File", "", "PGN Files (*.pgn);;All Files (*)")
@@ -84,22 +101,28 @@ class MainWindow(QMainWindow):
     def choose_variation(self):
         random_id = random.randrange(len(self.variations))
         self.variation = self.variations.pop(random_id)
-        print(f'Variation selected : {self.variation}')
+        print("Variation selected")
 
     def engine_turn(self):
         try:
             expected_move = self.variation[self.move_number - 1]
+            self.play_move_sound(expected_move)
+            # Update board status
+            self.board.push(expected_move)
             self.board_widget.play_move(expected_move)
+
             self.move_number += 1
             if self.move_number >= len(self.variation) and len(self.variation) !=0:
                 self.statusBar().showMessage("🎉 Line completed!", 3000)
-                self.reset_board()
+                QTimer.singleShot(5000, self.reset_board)
         except IndexError:
             self.reset_board()
 
     def reset_board(self):
         self.move_number = 1
+        self.board = chess.Board()
         self.board_widget.set_board(chess.Board())
+        print("Board Reset")
 
 
     def on_move_played(self, move: chess.Move, move_info: dict):
@@ -109,7 +132,12 @@ class MainWindow(QMainWindow):
             self.expected_move = self.variation[self.move_number - 1]
         except IndexError:
             pass
-        
+
+        self.play_move_sound(move)
+
+        # Update board status
+        self.board.push(move)
+
         print("Expected Move:", self.expected_move)
         print("User played:", move)
         if self.move_number%2 != 0:
@@ -117,8 +145,7 @@ class MainWindow(QMainWindow):
             if self.move_number >= len(self.variation) and len(self.variation) !=0:
                 self.streak += 1
                 self.statusBar().showMessage("🎉 Line completed!", 3000)
-                time(5000)
-                self.reset_board()
+                QTimer.singleShot(5000, self.reset_board)
             else:
                 # print(f'Expected_move : {self.expected_move}')
                 if move == self.expected_move:
@@ -131,11 +158,11 @@ class MainWindow(QMainWindow):
                     self.statusBar().showMessage("❌ Wrong move!", 2000)
                     # Undo move visually
                     self.board_widget.undo_move()
+                    self.board.pop()
         
     def show_move(self):
         try:
             self.engine_turn()
-            time(3000)
             self.engine_turn()
         except IndexError:
             pass
